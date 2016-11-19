9f73c6b28722db34573df77c4166f7bb8afce8ae
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2016-11-19 15:56:38 +0300

    Added comments

--- src/main/_SurnameName/bridgesInfo.kt
+++ src/main/_SurnameName/bridgesInfo.kt
@@ -8,6 +8,10 @@ import bridges.SwitchAction.CHANGE
 import bridges.SwitchType
 import bridges.SwitchType.HALF_BLOCK
 
+// Your solution should live in this folder/package only (rename _SurnameName accordingly.)
+// You may add as many subpackages as you need, but the function 'bridgesInfo' below should live in the root _SurnameName package.
+// Please DON'T copy the class 'BridgesInfo' and others here.
+
 fun bridgesInfo(init: () -> Unit): BridgesInfo = TODO()
 
 // These functions need receivers to works correctly. The declarations below are only used to have the compiled code.
